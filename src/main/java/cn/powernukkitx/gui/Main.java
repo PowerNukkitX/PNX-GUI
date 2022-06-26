0de01b311d83caa2d9a81839deb631868e8bb7d5
超神的冰凉 <2801621420@qq.com>
2022-06-26 18:10:56 +0800

    去掉css

--- src/main/java/cn/powernukkitx/gui/Main.java
+++ src/main/java/cn/powernukkitx/gui/Main.java
@@ -1,24 +1,35 @@
 package cn.powernukkitx.gui;
 
+import cn.powernukkitx.gui.util.LoggerUtils;
 import cn.powernukkitx.gui.window.MainWindow;
 import com.formdev.flatlaf.FlatDarculaLaf;
 import me.friwi.jcefmaven.CefInitializationException;
 import me.friwi.jcefmaven.UnsupportedPlatformException;
 
 import java.io.IOException;
+import java.util.logging.Filter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 public class Main {
     public static MainWindow mainWindow;
 
     public static void main(String[] args) {
         FlatDarculaLaf.setup();
+        doChores();
         try {
             mainWindow = new MainWindow(false, new String[0]);
             var welcomeWindow = mainWindow.addIndexPage("pnx://bundle/html/index.html", "image/welcome.svg", "欢迎", "PNX-GUI欢迎页");
             var aboutWindow = mainWindow.addIndexPage("pnx://bundle/html/about.html", "image/about.svg", "关于", "关于PNX-GUI");
             mainWindow.setVisible(true);
+            System.out.println("SBSBSB");
         } catch (UnsupportedPlatformException | CefInitializationException | IOException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void doChores() {
+
     }
 }
---
+++ src/main/java/cn/powernukkitx/gui/util/LoggerUtils.java
@@ -0,0 +1,31 @@
+package cn.powernukkitx.gui.util;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Filter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+public final class LoggerUtils {
+    private LoggerUtils() {
+
+    }
+
+    public static void forceDisableLogger(String name, String resourceBundle, Class<?> callerClass) {
+        try {
+            var method = Logger.class.getDeclaredMethod("getLogger", String.class, String.class, Class.class);
+            method.setAccessible(true);
+            var result = (Logger) method.invoke(null, name, resourceBundle, callerClass);
+            result.setLevel(Level.OFF);
+            result.setFilter(new Filter() {
+                @Override
+                public boolean isLoggable(LogRecord record) {
+                    System.out.println(record.getMessage());
+                    return false;
+                }
+            });
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+}
